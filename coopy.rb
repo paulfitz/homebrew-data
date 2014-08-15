28f6eae2ef1ec18d915abd4b3e3c71a50cf504a6
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-08-14 21:42:04 -0400

    update coopy version

--- coopy.rb
+++ coopy.rb
@@ -1,8 +1,9 @@
 require 'formula'
 
 class Coopy < Formula
-  url 'https://github.com/paulfitz/coopy/tarball/v0.5.5'
   homepage 'http://share.find.coop'
+  url 'https://github.com/paulfitz/coopy/archive/v0.6.7.tar.gz'
+  sha256 '7958e83f4161fade99406d8b9dd3e7fcc95e0337eed920f4f6bfe06a09d59150'
   head 'git://github.com/paulfitz/coopy.git'
 
   depends_on 'cmake' => :build
@@ -10,7 +11,6 @@ class Coopy < Formula
   depends_on 'mdbtools'
   depends_on 'mysql-connector-c'
   depends_on 'gnumeric'
-  depends_on 'gettext'
   depends_on 'wxmac'
 
   def install
